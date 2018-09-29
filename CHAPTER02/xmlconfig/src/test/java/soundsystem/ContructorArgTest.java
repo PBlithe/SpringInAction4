ec6a4c4a204afa46a3b60733234645e56511e449
chad <panxiaowei123456@gmail.com>
2018-09-29 15:51:21 +0800

    add xmlconfig

---
+++ CHAPTER02/xmlconfig/src/test/java/soundsystem/ContructorArgTest.java
@@ -0,0 +1,21 @@
+package soundsystem;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = "ConstructorArgTest.xml")
+public class ContructorArgTest {
+    @Autowired
+    private CompactDisc cd;
+    @Autowired
+    private MediaPlayer player;
+
+    @Test
+    public void play(){
+        player.play();
+    }
+}
