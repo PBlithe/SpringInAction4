a042a0a152bddb573cdad5bea3029f77fb5d66eb
chad <panxiaowei123456@gmail.com>
2018-10-08 15:12:43 +0800

    add conditional

---
+++ CHAPTER03/conditional/src/test/java/com/habuma/restfun/MagicExistsTest.java
@@ -0,0 +1,20 @@
+package com.habuma.restfun;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = MagicConfig.class)
+public class MagicExistsTest {
+    @Autowired
+    private ApplicationContext context;
+    @Test
+    public void shouldNotBeNull(){
+        //assertTrue(context.containsBean("magicBean"));
+        System.out.println("wo zui shuai");
+    }
+}
